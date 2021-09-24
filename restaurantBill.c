c4b0b00029ebe48b37b07ce323c3d0a802cbfee8
Justin <jko1@cpp.edu>
2021-09-23 18:06:04 -0700

    made an outline for part 1

---
+++ restaurantBill.c
@@ -0,0 +1,7 @@
+/*take in input from the command line. 
+The arguement at the first index is saved in a variable
+that stores the tax. 
+The second index is saved in a variable that stores the tip.
+Use a random number generator that generates a number 1-4
+Depending on the number, picks a meal and calculates the total cost.
+Prints the total cost
